510138a7f6d918cd3e7cf8db99309742d541418b
emmanuelandyeze <emmanuel.andyeze@gmail.com>
2025-07-07 16:18:00 +0100

    update form

--- app/agent/page.tsx
+++ app/agent/page.tsx
@@ -270,7 +270,7 @@ export default function BecomeAgent() {
 							required
 							className="w-full p-3 bg-white rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500"
 						>
-							<option value="">Select Qualification</option>{' '}
+							<option value="">Select</option>{' '}
 							{/* Default disabled option */}
 							<option value="SSCE">SSCE</option>
 							<option value="ND">ND</option>
